1ef6799b8be3e1fa37ad2b1f19de96cd6310128d
lijiahui <2836755307@qq.com>
2020-08-17 13:25:19 +0800

    解决eslint

--- src/api/user.js
+++ src/api/user.js
@@ -2,40 +2,40 @@ import service from '@/utils/request'
 /**
  * 获取验证码
  */
-export function GetSms(data){
-    return service.request({
-        method: "post",
-        url: "/getSms/",
-        data
-    })
+export function GetSms(data) {
+  return service.request({
+    method: 'post',
+    url: '/getSms/',
+    data
+  })
 }
 /**
  * 登录
  */
-export function Login(data){
+export function Login(data) {
   return service.request({
-      method: "post",
-      url: "/login/",
-      data
+    method: 'post',
+    url: '/login/',
+    data
   })
 }
 
 /**
  * 退出
  */
-export function Logout(data = {}){
-    return service.request({
-        method: "post",
-        url: "/logout/",
-        data
-    })
+export function Logout(data = {}) {
+  return service.request({
+    method: 'post',
+    url: '/logout/',
+    data
+  })
 }
 
 // 获取用户权限
-export function GetUserRoles(data = {}){
-    return service.request({
-        method: "post",
-        url: "/userRole/",
-        data
-    })
+export function GetUserRoles(data = {}) {
+  return service.request({
+    method: 'post',
+    url: '/userRole/',
+    data
+  })
 }
